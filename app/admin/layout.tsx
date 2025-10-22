5012ccf6ff198fa0c4c596deb4088d6aad653243
sadiaKamal <sadiakamal2468@gmail.com>
2025-10-23 01:14:01 +0500

    add redirect to landing page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -101,7 +101,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }: { sidebarOpen: boolean; setSid
       <div className="  lg:inset-y-0 lg:flex lg:w-full lg:h-full lg:flex-col">
         <div className="flex min-h-0 flex-1 flex-col ">
           <div className="flex h-16 items-center px-4">
-            <Link href="/admin" className="text-xl font-bold text-red-500">
+            <Link href="/" className="text-xl font-bold text-red-500">
               BHB ADMIN
             </Link>
           </div>
